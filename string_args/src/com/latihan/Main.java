7bc9ad79af07fb065c339e800eb22739611acb82
Achmad Maulana Assidik <achmad.assidik@brilife.co.id>
2024-07-04 09:13:56 +0700

    Be;ajar Java

---
+++ string_args/src/com/latihan/Main.java
@@ -0,0 +1,11 @@
+package com.latihan;
+
+public class Main {
+    
+    public static void main (String[] args){    // args adalah variabel berupa array dengan tipe data String, digunakan untuk menyimpan argumen saat menjalankan atau memanggil aplikasi
+
+        System.out.println("Hallo " + args[0]); // memanggil nilai args index ke-0, sehingga saat menjalankan program perlu menambahkan argumen, contoh java Main <argumen>
+
+    }
+
+}
