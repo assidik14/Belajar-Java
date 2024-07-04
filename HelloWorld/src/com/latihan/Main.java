cefe0db483c015c66bdb6e1a033b4834b135b2ff
Achmad Maulana Assidik <achmad.assidik@brilife.co.id>
2024-07-04 08:48:26 +0700

    Belajar Java

---
+++ HelloWorld/src/com/latihan/Main.java
@@ -0,0 +1,13 @@
+package com.latihan;
+
+public class Main {
+    
+    public static void main (String[] args){
+
+        System.out.println("Hello World !!");
+        System.out.print("This is my First ");
+        System.out.println("Java App");
+
+    }
+
+}
---
+++ print/src/com/latihanprint/Main.java
@@ -0,0 +1,21 @@
+package com.latihanprint;
+
+public class Main {
+    
+    public static void main (String[] args){
+
+        // Mencetak tanpa memasukkan baris baru
+        System.out.print("Tanpa baris baru");
+
+        // Mencetak dengan baris baru
+        System.out.println("Menggunakan baris baru");
+
+        // Mencetak dengan baris baru secara manual
+        System.out.print("Beris baru secara manual \n");
+
+        // Mencetak dengan format nomor di luar string
+        System.out.printf("Angka : %d \n", 100);
+
+    }
+
+}
